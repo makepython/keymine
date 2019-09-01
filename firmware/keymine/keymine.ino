a9ead45d0a5231bcd7c1c9ec2dc473eb6a74496f
Stefano Borini <stefano.borini@gmail.com>
2019-09-01 18:05:52 +0100

    Added keycodes and producing them

--- firmware/keymine/keymine.ino
+++ firmware/keymine/keymine.ino
@@ -2,25 +2,32 @@
 // Stefano Borini - 2019
 // from baldengineer.com
 // CC BY-SA 4.0
-  
-byte rows[] = {22, 24};
-byte cols[] = {38, 40};
+#include "usb_hid_keycodes.h"
 
-const int rowCount = sizeof(rows)/sizeof(rows[0]);
-const int colCount = sizeof(cols)/sizeof(cols[0]); 
+byte rowPins[] = {22, 24};
+byte colPins[] = {38, 40};
+
+const int rowCount = sizeof(rowPins)/sizeof(rowPins[0]);
+const int colCount = sizeof(colPins)/sizeof(colPins[0]); 
 byte keys[colCount][rowCount];
- 
+
+byte scanCodes[][rowCount] = {
+  {KEY_Q, KEY_W},
+  {KEY_A, KEY_S}
+};
+
+
 void setup() {
     Serial.begin(115200);
  
     for(int x=0; x<rowCount; x++) {
-        Serial.print(rows[x]); Serial.println(" as input");
-        pinMode(rows[x], INPUT);
+        Serial.print(rowPins[x]); Serial.println(" as input");
+        pinMode(rowPins[x], INPUT);
     }
  
     for (int x=0; x<colCount; x++) {
-        Serial.print(cols[x]); Serial.println(" as input-pullup");
-        pinMode(cols[x], INPUT_PULLUP);
+        Serial.print(colPins[x]); Serial.println(" as input-pullup");
+        pinMode(colPins[x], INPUT_PULLUP);
     }
          
 }
@@ -29,15 +36,15 @@ void readMatrix() {
     // iterate the columns
     for (int colIndex=0; colIndex < colCount; colIndex++) {
         // col: set to output to low
-        byte curCol = cols[colIndex];
+        byte curCol = colPins[colIndex];
         pinMode(curCol, OUTPUT);
         digitalWrite(curCol, LOW);
  
-        // row: interate through the rows
+        // row: interate through the rowPins
         for (int rowIndex=0; rowIndex < rowCount; rowIndex++) {
-            byte rowCol = rows[rowIndex];
+            byte rowCol = rowPins[rowIndex];
             pinMode(rowCol, INPUT_PULLUP);
-         keys[colIndex][rowIndex] = digitalRead(rowCol);
+            keys[colIndex][rowIndex] = digitalRead(rowCol);
             pinMode(rowCol, INPUT);
         }
         // disable the column
@@ -46,19 +53,13 @@ void readMatrix() {
 }
  
 void printMatrix() {
-    for (int rowIndex=0; rowIndex < rowCount; rowIndex++) {
-        if (rowIndex < 10)
-            Serial.print(F("0"));
-        Serial.print(rowIndex); Serial.print(F(": "));
- 
-        for (int colIndex=0; colIndex < colCount; colIndex++) {  
-            Serial.print(keys[colIndex][rowIndex]);
-            if (colIndex < colCount)
-                Serial.print(F(", "));
+    for (int colIndex=0; colIndex < colCount; colIndex++) {
+      for (int rowIndex=0; rowIndex < rowCount; rowIndex++) {
+            if (!keys[colIndex][rowIndex]) {
+              Serial.println(scanCodes[colIndex][rowIndex]);
+            }
         }   
-        Serial.println("");
     }
-    Serial.println("");
 }
  
 void loop() {
