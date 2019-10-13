56e92dbec738d34c0a6303d40b9141f570dd0d82
Stefano Borini <stefano.borini@gmail.com>
2019-10-13 11:51:09 +0100

    Modified the raspi code

--- firmware/raspiinterrupt.py
+++ firmware/raspiinterrupt.py
@@ -1,11 +1,17 @@
 import time
 import RPi.GPIO as GPIO
+import smbus
+
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
+bus = smbus.SMBus(1)
+address = 0x42
+
 def callback(channel):
-    print("interrupt")
+    data = bus.read_i2c_block_data(address, 0x00, 32)
+    print(data)
 
 
 GPIO.add_event_detect(4, GPIO.RISING, callback=callback)
