6c4ea026146915e26abcd08d684c3e67001d255a
Stefano Borini <stefano.borini@gmail.com>
2019-10-26 17:53:12 +0100

    moved raspi code to subdir:wq

--- firmware/keymine-raspi/raspiinterrupt.py
+++ firmware/keymine-raspi/raspiinterrupt.py
@@ -9,9 +9,20 @@ GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 bus = smbus.SMBus(1)
 address = 0x42
 
+scans = 0
+fails = 0
+
 def callback(channel):
-    data = bus.read_i2c_block_data(address, 0x00, 32)
-    print(data)
+    global scans
+    global fails
+    try:
+        data = bus.read_i2c_block_data(address, 0x00, 32)
+    except Exception as e:
+        print(e)
+        fails += 1
+    scans += 1
+    if (scans % 100) == 0:
+        print(scans, fails)
 
 
 GPIO.add_event_detect(4, GPIO.RISING, callback=callback)
